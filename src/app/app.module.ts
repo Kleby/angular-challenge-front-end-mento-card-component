b397329082cdc6abb7cfa80e9b10b53d24a9f2fc
Kleby <kleby10@gmail.com>
2023-05-17 20:40:18 -0300

    Iniciando o desafio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
