35fdfa61d990e8784a7efe8ce4353d01eee09627
Kleby <kleby10@gmail.com>
2023-05-18 21:13:17 -0300

    Desafio completado

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css', './card.responsive.component.css']
 })
-export class CardComponent {
+export class CardComponent implements OnInit{
 
+  @Input()
+  title: string = "";
+
+  @Input()
+  description: string = "";
+
+  @Input()
+  cardIcon: string = "";
+
+  @Input()
+  colorCard: string = "";  
+
+  @Input()
+  isFirst: boolean  = false;
+
+  @Input()
+  isLast: boolean = false;
+
+  constructor(){}
+  ngOnInit(): void {
+
+  }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css', './home.responsive.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  dataCards: any;
+
+  constructor(){}
+
+  async ngOnInit(){  
+
+    fetch('assets/front-end-mentor-files/data/card_data.json')
+    .then(res => res.json())
+    .then(data =>  this.dataCards =  data.map((el:any) => {
+      return el;
+    })) 
+    .catch((err) => console.error(`Error ao pega os dados: ${err} `)); 
+  }
 
 }
